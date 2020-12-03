11806bca7419beab1459d2296b555b739fbac64c
TimDel <tim.del44210@gmail.com>
2020-12-03 16:07:33 +0100

    ajout connectionDAL

---
+++ Solution/ConnectionDAL.cs
@@ -0,0 +1,31 @@
+using MySql.Data.MySqlClient;
+using System;
+
+namespace Solution
+{
+    class ConnectionDAL
+    {
+        private static string server;
+        private static string database;
+        private static string uid;
+        private static string password;
+        public static MySqlConnection connection;
+
+        public static MySqlConnection OpenConnection()
+        {
+            if (connection == null) //  si la connexion est déjà ouverte, il ne la refera pas 
+            {
+                server = "localhost";
+                database = "testvisualstudiob2";
+                uid = "bidcardcoin";
+                password = "bidcardcoin";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+            }
+            return connection;
+        }
+    }
+}
